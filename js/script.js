7d0c1472718e367dc5c831541498b9f649af12d2
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-06-20 18:57:15 -0500

    Started ABOUT page. Added jQuery effects for transition to ABOUT page.

--- js/script.js
+++ js/script.js
@@ -2,5 +2,12 @@ console.log('Loding....');
 
 $(document).ready(function() {
      // Stuff to do as soon as the DOM is ready
+     $("#about").click(function(){
+         $("div.logo_container").slideToggle("slow", function(){
+             $("div.about_container").fadeToggle(800, function() {
+                 //Stuff to do *after* the animation takes place
+             })
+         })
+     });
      console.log('Done Loading');
 });
