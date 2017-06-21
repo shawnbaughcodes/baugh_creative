b6c85bf351f857e4d6a1aa2c10c8af6cd5858727
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-06-21 16:20:32 -0500

    Added css and chenged for mat for Page. added hidden properties to html. erased jquery code and will replace with new functions

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ console.log('Loding....');
 $(document).ready(function() {
      // Stuff to do as soon as the DOM is ready
      $("#about").click(function(){
-         $("div.logo_container").slideToggle("slow", function(){
-             $("div.about_container").fadeToggle(800, function() {
+         $("div.logo_container").slideToggle(600, function(){
+             $("div.about_container").fadeIn(2000, function() {
                  //Stuff to do *after* the animation takes place
              })
          })
