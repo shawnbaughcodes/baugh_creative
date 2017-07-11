01be5c8613dca37ebdfcb6faf568775e34a20d65
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-07-11 18:05:53 -0500

    Edited styles to be responsive for each panel and screen size. Edited projects panel to have all projects and pictures for projects as well as links to see code and website.

--- static/js/script.js
+++ static/js/script.js
@@ -24,62 +24,62 @@ $(document).ready(function() {
     //  ****************FUNCTIONS****************
     //  about slide
     function about_slide() {
-        $(".container1").hide('slide', {direction: 'right'}, 1500);
-        window.setTimeout(about_fade,1500)
+        $(".container1").hide('slide', {direction: 'right'}, 800);
+        window.setTimeout(about_fade,800)
         function about_fade() {
-            $(".container2").fadeToggle(2000, function() {
+            $(".container2").fadeToggle(800, function() {
             })
         }
     }
     // exit about
     function exit_about() {
-        $(".container2").fadeToggle(1000, function() {
+        $(".container2").fadeToggle(800, function() {
             //Stuff to do *after* the animation takes place
         })
-        window.setTimeout(exit_about,1500)
+        window.setTimeout(exit_about,800)
         function exit_about() {
-            $(".container1").show('slide', {direction: 'right'}, 1500);
+            $(".container1").show('slide', {direction: 'right'}, 800);
         }
     }
 
     // project slide
     function projects_slide() {
-        $(".container1").hide('slide', {direction: 'left'}, 1500);
-        window.setTimeout(project_show,1500)
+        $(".container1").hide('slide', {direction: 'left'}, 800);
+        window.setTimeout(project_show,800)
         function project_show() {
-            $(".container3").fadeToggle(2000, function() {
+            $(".container3").fadeToggle(800, function() {
                 //Stuff to do *after* the animation takes place
             })
         }
     }
     // exit project
     function exit_projects() {
-        $(".container3").fadeToggle(1000, function() {
+        $(".container3").fadeToggle(800, function() {
             //Stuff to do *after* the animation takes place
         })
-        window.setTimeout(exit_project, 1500)
+        window.setTimeout(exit_project, 800)
         function exit_project() {
-            $(".container1").show('slide', {direction: 'left'}, 1500)
+            $(".container1").show('slide', {direction: 'left'}, 800)
         }
     }
     // contact slide
     function contact_slide() {
-        $(".container1").hide('slide', {direction:'up'}, 1500);
-        window.setTimeout(contact_show, 1500)
+        $(".container1").hide('slide', {direction:'up'}, 800);
+        window.setTimeout(contact_show, 800)
         function contact_show() {
-            $(".container4").fadeToggle(1500, function() {
+            $(".container4").fadeToggle(800, function() {
                 //Stuff to do *after* the animation takes place
             })
         }
     }
     // exit contact
     function exit_contact() {
-        $('.container4').fadeToggle(1000, function() {
+        $('.container4').fadeToggle(800, function() {
             //Stuff to do *after* the animation takes place
         })
-        window.setTimeout(slide_out, 1500)
+        window.setTimeout(slide_out, 800)
         function slide_out() {
-            $(".container1").show('slide', {direction: 'up'}, 1500)
+            $(".container1").show('slide', {direction: 'up'}, 800)
         }
     }
      console.log('Done Loading');
